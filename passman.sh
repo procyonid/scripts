70f19e20278938ffca149f6371f1d9dc6cbf1122
Raccoon <procyonid@member.fsf.org>
2021-12-21 18:21:53 +0100

    passman: now properly trimming password entries from 'fd'

--- passman.sh
+++ passman.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-account=$(fd -t f --base-directory ~/.password-store/ ".*.gpg$" | grep -v "protonmail-credentials" | rofi -theme dt-dmenu.rasi -dmenu -p "Choose: " )
+account=$(fd -t f --base-directory ~/.password-store/ ".*.gpg$" | grep -v "protonmail-credentials" | cut -c 3- | rofi -theme dt-dmenu.rasi -dmenu -p "Choose: " )
 
 [ -z "$account" ] && exit
 
