151b0311a4ab2525a7acad8f1b14ad7cae82a889
Raccoon <procyonid@member.fsf.org>
2021-12-22 21:36:59 +0100

    mnemonify: non-number characters are now replaced with spaces; fixed translation of 6's

--- mnemonify.sh
+++ mnemonify.sh
@@ -4,7 +4,7 @@ text=$1
 
 [ -z "$text" ] && read text
 
-text=$(echo $text | sed 's/[^0-9]*//g')
+text=$(echo $text | sed 's/[^0-9]/ /g')
 
 text=$(echo $text | tr '0' 's')
 text=$(echo $text | tr '1' 't')
@@ -12,7 +12,7 @@ text=$(echo $text | tr '2' 'n')
 text=$(echo $text | tr '3' 'm')
 text=$(echo $text | tr '4' 'r')
 text=$(echo $text | tr '5' 'l')
-text=$(echo $text | tr '6' 'sh')
+text=$(echo $text | sed 's/6/sh/g')
 text=$(echo $text | tr '7' 'k')
 text=$(echo $text | tr '8' 'f')
 text=$(echo $text | tr '9' 'p')
